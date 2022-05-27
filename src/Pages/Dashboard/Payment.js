6c0104a3e37bf4449972ec77029d805c8890d3cf
tanimtonni12 <17103062iubat@gmail.com>
2022-05-27 21:33:59 +0600

    payment is updated

--- src/Pages/Dashboard/Payment.js
+++ src/Pages/Dashboard/Payment.js
@@ -1,9 +1,12 @@
 import React from 'react';
-
+import { loadStripe } from '@stripe/stripe-js';
+import { Elements } from '@stripe/react-stripe-js';
 import { useQuery } from 'react-query';
 import { useParams } from 'react-router-dom';
 
 import Loading from '../Shared/Loading';
+import CheckoutForm from './CheckoutForm';
+const stripePromise = loadStripe('pk_test_51L43myCZVrcgDcnfYfDV0Gr9e5jOVc6DDDdDTiOlhx2tLRuhWF8rnn7KvsCVox2qg5DMAwRjFCFfRrE80yqIb41j00MRaTSimB');
 
 const Payment = () => {
     const { id } = useParams();
@@ -23,15 +26,15 @@ const Payment = () => {
             <div class="card w-50 max-w-md bg-base-100 shadow-xl my-12">
                 <div class="card-body">
                     <p className="text-primary font-bold">Hello, {order.name}</p>
-                    <h2 class="card-title">Please Pay for {order.ProductName}</h2>
+                    <h2 class="card-title">Please Pay for {order.productName}</h2>
                     <p>Please pay: $ {parseFloat(order.order_quantity) * parseFloat(order.per_product_price)}</p>
                 </div>
             </div>
             <div class="card flex-shrink-0 w-50 max-w-md shadow-2xl bg-base-100">
                 <div class="card-body">
-                    {/* <Elements stripe={stripePromise}>
-                        <CheckoutForm appointment={appointment} />
-                    </Elements> */}
+                    <Elements stripe={stripePromise}>
+                        <CheckoutForm order={order} />
+                    </Elements>
                 </div>
             </div>
         </div>
